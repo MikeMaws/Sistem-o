f6a37aec76d4319ccbcffcebcdef24a9b043c58c
MikeMaws <mike.maws@outlook.com>
2017-06-23 15:39:10 +0000

    correção login

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,10 @@ Rails.application.routes.draw do
   get 'alunos/index'
   get 'professores/index'
   get 'administradores/index'
-  root "index#index"
   devise_for :users
- 
- #devise_scope :user do
-  #root :to => 'devise/sessions#new'
- #end
-  
-   
+  devise_scope :user do
+  root :to => 'devise/sessions#new'
+ end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
